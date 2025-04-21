b41027f0e999367a68d294ce403053cae78581f9
and.ajs <123419010+ajchasiloa@users.noreply.github.com>
2025-04-21 14:33:55 -0500

    [ADD] import_dash: Add new module (#4)

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from . import models
-<<<<<<< HEAD
-from . import wizards  # Asegurando que se importe el directorio wizards
-=======
-from . import wizards
->>>>>>> 58ff627 (Actualización en account_move.py y account_move_view.xml)
